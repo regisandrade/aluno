beea5a4a10b254bc7684d5d811810202cafd1f96
Regis <regisandrade@gmail.com>
2013-04-10 14:36:58 -0300

    alterando os arquivos

--- exercicios/exercicio.php
+++
@@ -1,4 +0,0 @@
-<h2>Material disponí­vel</h2>
-
-<input type="button" name="btnAtual" value="TURMA ATUAL" class="botao" onClick="JavaScript: Direcionar(this.value)" />
-<input type="button" name="btnAnterior" value="TURMAS ANTERIORES" class="botao" onClick="JavaScript: Direcionar(this.value)" />
