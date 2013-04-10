348210554716939493fc2470117018390b9a8d46
Regis <regisandrade@gmail.com>
2013-04-10 14:38:34 -0300

    segundo commit - reestruturando

---
+++ principal.php
@@ -0,0 +1,24 @@
+      <!-- Main hero unit for a primary marketing message or call to action -->
+      <div class="hero-unit">
+        <h1>Nome do aluno</h1>
+        <p>Um local que o IPECON oferece aos alunos para interagir com os seus alunos.</p>        
+      </div>
+
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="span4">
+          <h2>Avisos</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn" href="#">Mais avisos &raquo;</a></p>
+        </div>
+        <div class="span4">
+          <h2>Vagas</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn" href="#">Mais vagas &raquo;</a></p>
+       </div>
+        <div class="span4">
+          <h2>Cronograma</h2>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          <p><a class="btn" href="#">Mais cronogramas &raquo;</a></p>
+        </div>
+      </div>
