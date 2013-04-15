29eae37d058c7e327e8a9fb2d97a48eb5abd5f6a
Regis <regisandrade@gmail.com>
2013-04-15 17:31:07 -0300

    alterando modulo senha e criando a classe senha

--- alterarSenha/index.php
+++ alterarSenha/index.php
@@ -1,13 +1,13 @@
 <h2>Alterar senha</h2>
 
-<form name="form_senha" method="post" action="senha/senha_alterada.php" onSubmit="return VerificarFormSenha()">
-  <input name="login" type="hidden" value="<?php print($_SESSION['login']); ?>">
+<form name="form_senha" method="post" action="senha/senha_alterada.php">
+  <input name="login" type="hidden" value="<?php echo $_SESSION['login']; ?>">
 
-  <label>Senha antiga:</label><input name="senha_antiga" type="password" class="txtInscricaoPequeno" id="senha_antiga" size="15" maxlength="15">
+  <label>Senha antiga:</label><input name="senha_antiga" type="password" id="senha_antiga">
 
-  <label>Nova senha:</label><input name="nova_senha" type="password" class="txtInscricaoPequeno" id="nova_senha" size="15" maxlength="15">
+  <label>Nova senha:</label><input name="nova_senha" type="password" id="nova_senha">
 
-  <label>Repetir senha:</label><input name="repetir_senha" type="password" class="txtInscricaoPequeno" id="repetir_senha" size="15" maxlength="15">
+  <label>Repetir senha:</label><input name="repetir_senha" type="password" id="repetir_senha">
 
   <button class="btn btn-large btn-primary" type="submit">Alterar Senha</button>
 
---
+++ class/senha.class.php
@@ -0,0 +1,45 @@
+<?php
+class Senha{
+	public function alterar($pdo,$parametros=null){
+		try {
+			$sql = "UPDATE usuario_aluno SET 
+                              Senha = ? 
+                       WHERE
+			               Login = ?";
+			
+			$rs = $pdo->prepare($sql);
+          	$count = $rs->execute(array($parametros['Senha']
+                                          ,$parametros['Login']));
+          	
+          	//var_dump($count, $rs->errorInfo());
+
+          	if($count === false){
+          		$resposta['mensagem'] = "Nenhum registro encontrado.";
+          		$resposta['sucesso'] = false;
+          	}else{
+          		$conta = 0;
+          		$arrDados = array();
+          		while ($registro = $rs->fetch(PDO::FETCH_OBJ)) {
+          			$arrDados[$conta]['Tipo'] = $registro->Tipo;
+          			$arrDados[$conta]['Link'] = $registro->Link;
+          			$arrDados[$conta]['Descricao'] = $registro->Descricao;
+
+          			$conta++;
+          		}
+
+          		$resposta['valores'] = $arrDados;
+          		$resposta['sucesso'] = true;
+          	}
+
+		} catch (Exception $e) {
+			$resposta['mensagem'] = $e;
+          	$resposta['sucesso'] = false;
+		}
+
+		$pdo = null;
+		return $arrDados;
+		//echo json_encode($resposta);
+	}
+
+}
+?>
