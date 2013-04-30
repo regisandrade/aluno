92073248182980be2a0d1fd93748526fd2eaf31c
Regis Andrade <regisandrade@gmail.com>
2013-04-29 21:57:05 -0300

    Não precisa passar o nome do usuário que esta recebendo a imagem

--- ajuda/enviar.php
+++ ajuda/enviar.php
@@ -4,10 +4,9 @@ $util = new Util();
 
 if (!empty($_REQUEST['nome']) && !empty($_REQUEST['email']) && !empty($_REQUEST['para']) && !empty($_REQUEST['mensagem'])) {
 	
-	$parametros['html']      = "SIM";
+    $parametros['html']      = "SIM";
     $parametros['assunto']   = "Mensagem enviada pela área do aluno";
     $parametros['mensagem']  = utf8_decode($_REQUEST['mensagem']);
-    $parametros['nomePara']  = $_REQUEST['para'];
     $parametros['emailPara'] = $_REQUEST['para'];
     $parametros['nomeFrom']  = $_REQUEST['nome'];
     $parametros['emailFrom'] = $_REQUEST['email'];
@@ -17,15 +16,15 @@ if (!empty($_REQUEST['nome']) && !empty($_REQUEST['email']) && !empty($_REQUEST[
     // echo "</pre>";
 	
     if($util->enviarEmail($parametros)){
-		$resposta['mensagem'] = "Mensagem enviada com sucesso.";
+        $resposta['mensagem'] = "Mensagem enviada com sucesso.";
         $resposta['sucesso']  = true;
-	}else{
-		$resposta['mensagem'] = "Erro ao enviar a mensagem.";
+    }else{
+        $resposta['mensagem'] = "Erro ao enviar a mensagem.";
         $resposta['sucesso']  = false;
-	}
+    }
 
 }else{
-	$resposta['mensagem'] = "Por favor, preencher todos os campos para enviar a mensagem.";
+    $resposta['mensagem'] = "Por favor, preencher todos os campos para enviar a mensagem.";
     $resposta['sucesso']  = false;
 }
 echo json_encode($resposta);
