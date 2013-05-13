9a82bf26b5aa52f3d525439cf43fffdc601ea7c5
Regis Andrade <regisandrade@gmail.com>
2013-05-13 14:30:22 -0300

    Aviso: mudar a classe de pesquisa para trazer um limite de registro e/ou um unico aviso. Cronograma, criado um metodo vaiso para trazer apenas a agenda do mês atual

--- class/aviso.class.php
+++ class/aviso.class.php
@@ -1,15 +1,33 @@
 <?php
 class Aviso{
+
+     /**
+     * Pesquisa generica para pesquisa de avisos:
+     *   -> Pode pesquisar por codigoAviso
+     *   -> Pode limitar a quantidade de registro para retornar
+     *
+     *
+     */
 	public function pesquisar($pdo,$parametros=null){
 		try {
 			$sql = "SELECT 
-					       Codg_Aviso
-					      ,Titulo
-					      ,Descricao
-					      ,DATE_FORMAT(Data_Cadastro,'%d/%m/%Y') AS Data 
-					FROM 
-					       aviso 
-					ORDER BY Codg_Aviso";
+                              Codg_Aviso
+                             ,Titulo
+                             ,Descricao
+                             ,DATE_FORMAT(Data_Cadastro,'%d/%m/%Y') AS Data 
+                       FROM 
+                              aviso ";
+               
+               if($parametros['codigoAviso']){
+                   $sql .= "WHERE 
+                              Codigo_Aviso = ".$parametros['codigoAviso'];
+               }
+               if($parametros['numLimite']){
+                   $sql .= "LIMIT ".$parametros['numLimite'];
+               }
+                       
+               $sql .= "ORDER BY 
+                              Codg_Aviso";
 			
 			$rs = $pdo->prepare($sql);
           	$count = $rs->execute();
--- class/cronograma.class.php
+++ class/cronograma.class.php
@@ -1,5 +1,6 @@
 <?php
 class Cronograma{
+
 	public function pesquisar($pdo,$parametros=null){
 		try {
 			$sql = "SELECT
@@ -18,6 +19,58 @@ class Cronograma{
 					       CRO.Turma = ?
 					ORDER BY
 					       CRO.Data_01, CRO.Data_02, CRO.Data_03, CRO.Data_04, CRO.Data_05, CRO.Data_06 DESC";
+			
+			$rs = $pdo->prepare($sql);
+          	$count = $rs->execute(array($parametros['turma']));
+          	
+          	//var_dump($count, $rs->errorInfo());
+
+          	if(!$rs){
+          		$resposta['mensagem'] = "Nenhum registro encontrado.";
+          		$resposta['sucesso'] = false;
+          	}else{
+          		$conta = 0;
+          		$arrDados = array();
+          		while ($registro = $rs->fetch(PDO::FETCH_OBJ)) {
+          			$arrDados[$conta]['Disciplina'] = $registro->Disciplina;
+          			$arrDados[$conta]['Data_1'] = ($registro->Data_1 != '00/00/0000' ? $registro->Data_1 : '--');
+          			$arrDados[$conta]['Data_2'] = ($registro->Data_2 != '00/00/0000' ? $registro->Data_2 : '--');
+          			$arrDados[$conta]['Data_3'] = ($registro->Data_3 != '00/00/0000' ? $registro->Data_3 : '--');
+          			$arrDados[$conta]['Data_4'] = ($registro->Data_4 != '00/00/0000' ? $registro->Data_4 : '--');
+          			$arrDados[$conta]['Data_5'] = ($registro->Data_5 != '00/00/0000' ? $registro->Data_5 : '--');
+          			$arrDados[$conta]['Data_6'] = ($registro->Data_6 != '00/00/0000' ? $registro->Data_6 : '--');
+
+          			$conta++;
+          		}
+
+          		$resposta['valores'] = $arrDados;
+          		$resposta['sucesso'] = true;
+          	}
+
+		} catch (Exception $e) {
+			$resposta['mensagem'] = $e;
+          	$resposta['sucesso'] = false;
+		}
+
+		$pdo = null;
+		return $arrDados;
+		//echo json_encode($resposta);
+	}
+
+	/**
+	* Listar todas os calendários do mês atual
+	*
+	* @param array $parametros
+	*
+	*/
+	public function pesquisarAgendaMesAtual($pdo, $parametros=null){
+				try {
+			$sql = "SELECT
+					       *
+					FROM
+					       cronograma 
+					WHERE
+					       Mes = ?";
 			
 			$rs = $pdo->prepare($sql);
           	$count = $rs->execute(array($parametros['turma']));
