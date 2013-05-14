0632337a53144cb944895750075cc16a1f4be7b6
Regis Andrade <regisandrade@gmail.com>
2013-05-14 15:40:12 -0300

    Corrigindo LIMIT na consulta de avisos

--- class/aviso.class.php
+++ class/aviso.class.php
@@ -11,24 +11,25 @@ class Aviso{
 	public function pesquisar($pdo,$parametros=null){
 		try {
 			$sql = "SELECT 
-                              Codg_Aviso
-                             ,Titulo
-                             ,Descricao
-                             ,DATE_FORMAT(Data_Cadastro,'%d/%m/%Y') AS Data 
-                       FROM 
-                              aviso ";
+                    Codg_Aviso
+                   ,Titulo
+                   ,Descricao
+                   ,DATE_FORMAT(Data_Cadastro,'%d/%m/%Y') AS Data 
+             FROM 
+                    aviso \n";
                
-               if($parametros['codigoAviso']){
-                   $sql .= "WHERE 
-                              Codigo_Aviso = ".$parametros['codigoAviso'];
-               }
-               if($parametros['numLimite']){
-                   $sql .= "LIMIT ".$parametros['numLimite'];
-               }
-                       
-               $sql .= "ORDER BY 
-                              Codg_Aviso";
-			
+             if($parametros['codigoAviso']){
+                 $sql .= "WHERE 
+                            Codigo_Aviso = ".$parametros['codigoAviso'];
+             }
+                     
+             $sql .= "\nORDER BY 
+                            Codg_Aviso";
+
+             if($parametros['numLimite']){
+                 $sql .= " \n LIMIT ".$parametros['numLimite'];
+             }
+
 			$rs = $pdo->prepare($sql);
           	$count = $rs->execute();
           	
